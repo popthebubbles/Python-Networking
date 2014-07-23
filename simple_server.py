e447efcf0e20f9df7453f3bcb5bada62f3b2ae2f
Matt <bappy13@hotmail.com>
2014-07-23 14:59:55 -0400

    added simple_server.py

---
+++ simple_server.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+
+HOST = ''
+PORT = 8888 #Abitrary Port
+
+#creates a socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket Created'
+
+try:
+    #binds to a port
+    s.bind((HOST, PORT))
+except socket.error, msg:
+    print 'Failed to bind. Error Code: ' + str(msg[0]) + '. Error Message: ' + str(msg[1])
+    sys.exit()
+
+print 'Socket bind complete'
+
+s.listen(10) #tells to listen and sets backlog
+print 'Socket now listening'
+
+#wait to accept a connection- blocking call
+conn, addr = s.accept()
+
+#display client information
+print 'Connected with ' + addr[0] + ':' + str(addr[1])
